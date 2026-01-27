43b3410f08de0a145bd255257349324836b1b871
Benedito Paiva Brasil Filho <paivarobin16@gmail.com>
2026-01-27 19:05:15 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/app.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#0b0d12",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 24,
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      🚀 Viracheck AI carregou com sucesso!
+    </div>
+  );
+}
+
+export default App;
