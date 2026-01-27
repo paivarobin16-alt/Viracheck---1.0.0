cd7b1747c517943c0b030274e4f055068d3fd150
Benedito Paiva Brasil Filho <paivarobin16@gmail.com>
2026-01-27 05:27:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
+import Analyze from "./pages/Analyze";
+
 export default function App() {
-  return (
-    <div style={{ padding: 16, fontFamily: "system-ui" }}>
-      <h1>React renderizou ✅</h1>
-      <p>Se você está vendo isso, o Vercel está servindo o app corretamente.</p>
-    </div>
-  );
+  return <Analyze />;
 }
