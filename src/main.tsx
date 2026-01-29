c525a2f3e462ed71ed95636651d6d3dbfeacf239
Benedito Paiva Brasil Filho <paivarobin16@gmail.com>
2026-01-29 14:21:52 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error("Elemento #root não encontrado no index.html");
+}
+
+ReactDOM.createRoot(container).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
