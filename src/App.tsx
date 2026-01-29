e51a1cd5486104fa6aced120769ac6c2b8d88d27
Benedito Paiva Brasil Filho <paivarobin16@gmail.com>
2026-01-29 17:06:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
 import Analyze from "./pages/Analyze";
-import Privacy from "./pages/Privacy";
 import Footer from "./components/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div
-        style={{
-          minHeight: "100vh",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        {/* Conteúdo principal */}
-        <div style={{ flex: 1 }}>
-          <Routes>
-            <Route path="/" element={<Analyze />} />
-            <Route path="/privacy" element={<Privacy />} />
-          </Routes>
-        </div>
-
-        {/* Rodapé (NÃO interfere no app) */}
-        <Footer />
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {/* Conteúdo principal */}
+      <div style={{ flex: 1 }}>
+        <Analyze />
       </div>
-    </BrowserRouter>
+
+      {/* Rodapé (não interfere no app) */}
+      <Footer />
+    </div>
   );
 }
